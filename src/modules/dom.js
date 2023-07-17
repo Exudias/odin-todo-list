d7cbd5b692e734c9f51db5f177ad262232b580c4
Exudias <editoo31@gmail.com>
2023-07-17 14:16:07 +0300

    Make createTodoItem function return button references too

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -29,7 +29,7 @@ class DomManager
         const todoRemoveButtonContainer = createTodoButtonContainer("X");
         li.appendChild(todoRemoveButtonContainer);
 
-        return li;
+        return {li, todoCompleteButtonContainer, todoRemoveButtonContainer};
 
         function createTodoButtonContainer(buttonText)
         {
